19136e2b8650b617fab566d228989318086b7e2b
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-02-19 11:30:36 +0200

    pangrams

--- PangramsSolution/Program.cs
+++ PangramsSolution/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-namespace PangramsSolution
+namespace Solution
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Solution
+	{
+		static void Main(string[] args)
+		{
+			string pangram = "pangram";
+			string notpangram = "not pangram";
+			string pattern = "[a-zA-Z]";
+
+			string input = Console.ReadLine();
+
+			string text = input.ToLower();
+
+			HashSet<int> userChars = new HashSet<int>();
+
+			foreach (var ch in text)
+			{
+				if (userChars.Count == 26)
+				{
+					break;
+				}
+
+				if (Regex.IsMatch(ch.ToString(), pattern))
+				{
+					userChars.Add(ch);
+				}
+			}
+
+			Console.WriteLine(userChars.Count == 26 ? pangram : notpangram);
+		}
+	}
 }
