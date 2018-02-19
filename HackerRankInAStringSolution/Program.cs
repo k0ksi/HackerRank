2d123e1add65faef327231e00684c3f046e1bf4e
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-02-19 11:22:26 +0200

    HackerRank in a String!

--- HackerRankInAStringSolution/Program.cs
+++ HackerRankInAStringSolution/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
-
-namespace HackerRankInAStringSolution
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+	static string hackerrankInString(string s)
+	{
+		string yes = "YES";
+		string no = "NO";
+
+		string initial = "hackerrank";
+
+		int index = 0;
+		char current = initial[index];
+
+		foreach (var ch in s)
+		{
+			if (index == 10)
+			{
+				break;
+			}
+
+			current = initial[index];
+
+			if (ch == current)
+			{
+				index++;
+			}
+		}
+
+		string result = index == 10 ? yes : no;
+
+		return result;
+	}
+
+	private static bool CheckIfStringIsHackerRank(string s)
+	{
+		bool result = false;
+
+		if(String.Equals(s, "hackerrank", StringComparison.OrdinalIgnoreCase))
+		{
+			result = true;
+		}
+
+		return result;
+	}
+
+	static void Main(String[] args)
+	{
+		int q = Convert.ToInt32(Console.ReadLine());
+		for (int a0 = 0; a0 < q; a0++)
+		{
+			string s = Console.ReadLine();
+			string result = hackerrankInString(s);
+			Console.WriteLine(result);
+		}
+	}
 }
