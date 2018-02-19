f06b9aa3268e02f19f5d0e80a1122618d755b019
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-02-19 11:04:20 +0200

    Mars Exploration

--- MarsExplorationSolution/Program.cs
+++ MarsExplorationSolution/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Text;
 
-namespace MarsExplorationSolution
+class Solution
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+
+	static int marsExploration(string s)
+	{
+		int sosMessagesCount = s.Length / 3;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < sosMessagesCount; i++)
+		{
+			sb.Append("SOS");
+		}
+
+		var compare = sb.ToString();
+
+		int result = 0;
+
+		for (int i = 0; i < s.Length; i++)
+		{
+			var originalChar = s[i];
+			var charToCompare = compare[i];
+
+			if(originalChar != charToCompare)
+			{
+				result++;
+			}
+		}
+
+		return result;
+	}
+
+	static void Main(String[] args)
+	{
+		string s = Console.ReadLine();
+		int result = marsExploration(s);
+		Console.WriteLine(result);
+	}
 }
