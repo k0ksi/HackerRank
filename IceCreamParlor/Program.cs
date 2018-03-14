24ff92aedc1cb68641e3a253118c3d770761f37c
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-03-14 12:41:52 +0200

    - search algorithms

---
+++ IceCreamParlor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Solution
+{
+    static int[] icecreamParlor(int m, int[] arr)
+    {
+        var result = new int[2];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var first = arr[i];
+
+            for (int j = 1; j < arr.Length; j++)
+            {
+                if(i == j)
+                {
+                    continue;
+                }
+
+                var second = arr[j];
+
+                if(first + second == m)
+                {
+                    result[0] = i + 1;
+                    result[1] = j + 1;
+                }
+            }
+        }
+
+        Array.Sort(result);
+
+        return result;
+    }
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            int[] result = icecreamParlor(m, arr);
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
---
+++ MissingNumbers/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    static int[] missingNumbers(int[] arr, int[] brr)
+    {
+        var distinct = new HashSet<int>();
+
+        var firstDict = new Dictionary<int, int>();
+        var secondDict = new Dictionary<int, int>();
+
+        foreach (var item in arr)
+        {
+            distinct.Add(item);
+
+            if(firstDict.ContainsKey(item))
+            {
+                firstDict[item]++;
+            }
+            else
+            {
+                firstDict.Add(item, 1);
+            }
+        }
+
+        foreach (var item in brr)
+        {
+            distinct.Add(item);
+
+            if (secondDict.ContainsKey(item))
+            {
+                secondDict[item]++;
+            }
+            else
+            {
+                secondDict.Add(item, 1);
+            }
+        }
+
+        var missing = new HashSet<int>();
+
+        foreach (var item in distinct)
+        {
+            if(firstDict[item] != secondDict[item])
+            {
+                missing.Add(item);
+            }
+        }
+
+        var result = missing.ToArray();
+
+        Array.Sort(result);
+
+        return result;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        int m = Convert.ToInt32(Console.ReadLine());
+        string[] brr_temp = Console.ReadLine().Split(' ');
+        int[] brr = Array.ConvertAll(brr_temp, Int32.Parse);
+        int[] result = missingNumbers(arr, brr);
+        Console.WriteLine(String.Join(" ", result));
+    }
+}
