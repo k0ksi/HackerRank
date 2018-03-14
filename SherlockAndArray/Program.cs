61028de881f83f3d691f736fc20ba9b68209eda9
zhelyazko.stoyanov <stoyanov.zhelyazko@gmail.com>
2018-03-14 14:07:35 +0200

    Sherlock and Array

---
+++ SherlockAndArray/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    static string solve(int[] a)
+    {
+        bool exists = false;
+
+        //var firstSum = 0;
+        //var secondSum = 0;
+
+        int sum = a.Sum();
+
+        var currentSum = 0;
+
+        if(a.Length == 1)
+        {
+            return "YES";
+        }
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (i == 44)
+            {
+
+            }
+            var currentNum = a[i];
+            currentSum += a[i];
+
+            if (i == 0)
+                continue;
+
+            var newSum = sum - a[i];
+
+            if(newSum / 2 == currentSum - currentNum)
+            {
+                exists = true;
+                break;
+            }
+            
+            //if(i == 0)
+            //{
+            //    continue;
+            //}
+
+            //if(firstSum == secondSum)
+            //{
+            //    exists = true;
+            //    break;
+            //}
+        }
+
+        return exists ? "YES" : "NO";
+    }
+
+    static void Main(String[] args)
+    {
+        int T = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < T; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            string result = solve(a);
+            Console.WriteLine(result);
+        }
+    }
+}
